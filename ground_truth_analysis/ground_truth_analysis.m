372270ab8f01b2a0b92f6593e6dbf32c2cf6ca22
Torrey <salmont3@gmail.com>
2017-03-10 16:47:29 -0500

    celll attached analysis

--- ground_truth_analysis/ground_truth_analysis.m
+++ ground_truth_analysis/ground_truth_analysis.m
@@ -3,7 +3,7 @@
 %Plot df_f0 of patched cell
 figure;
 hold on
-for j = 1:size(tadpole.df_f0,1)
+for j = 1:size(tadpole.df_f0,2)
     plot(tadpole.df_f0{2,j})
 end
 hold off
@@ -11,7 +11,7 @@ ax=gca;
 xsize = length(tadpole.df_f0{1,j});
 ax.XTick = [0, xsize/7, (xsize/7)*2, (xsize/7)*3, (xsize/7)*4, (xsize/7)*5, (xsize/7)*6, (xsize/7)*7];
 ax.XTickLabel = {'0','1', '2', '3', '4', '5', '6', '7'};
-title(sprintf('Exp %d cell 2 all trials roi2', tadpole.expnum));
+title(sprintf('Exp %d tad2 cell1 all trials roi1', tadpole.expnum));
 xlabel('time(s)');
 ylabel('\Delta F/F_{0}');
 
@@ -25,7 +25,7 @@ end
 % plot smoothed data
 figure;
 hold on
-for j = 1:size(tadpole.filtered,1)
+for j = 1:size(tadpole.filtered,2)
     plot(tadpole.filtered{2,j})
 end
 hold off
@@ -33,6 +33,6 @@ ax=gca;
 xsize = length(tadpole.filtered{1,j});
 ax.XTick = [0, xsize/7, (xsize/7)*2, (xsize/7)*3, (xsize/7)*4, (xsize/7)*5, (xsize/7)*6, (xsize/7)*7];
 ax.XTickLabel = {'0','1', '2', '3', '4', '5', '6', '7'};
-title(sprintf('Exp %d cell 2 all trials, smoothed roi2', tadpole.expnum));
+title(sprintf('Exp %d tad2cell1 all trials, smoothed roi1', tadpole.expnum));
 xlabel('time(s)');
 ylabel('\Delta F/F_{0}');
