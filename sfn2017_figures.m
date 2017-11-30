bbb458415eff24b223a3221f624f1edaab3e9ad2
Torrey <salmont3@gmail.com>
2017-11-30 16:52:57 -0500

    sfn 2017

--- sfn2017_figures.m
+++ sfn2017_figures.m
@@ -10,9 +10,10 @@ for t = 1:length(allData)
         if isfield(allData{1,t}, 'correlated_ROIs_dff0_MS_common_AROI')
             figure;
             hold on
-            scatter(allData{1,t}.ROIcenters(allData{1,t}.resp_ROIs, 1), allData{1,t}.ROIcenters(allData{1,t}.resp_ROIs, 2), 65, 'o', 'MarkerEdgeColor', [0.5 0.5 0.5])
-            scatter(allData{1,t}.ROIcenters(allData{1,t}.uniqueHighCorrROI, 1), allData{1,t}.ROIcenters(allData{1,t}.uniqueHighCorrROI, 2), 100, 'o', 'filled', 'MarkerEdgeColor', [0.5 0 0.5], 'MarkerFaceColor', [0.5 0 0.5])
+            scatter(allData{1,t}.ROIcenters(allData{1,t}.resp_ROIs, 1), allData{1,t}.ROIcenters(allData{1,t}.resp_ROIs, 2), 150, 'o', 'filled', 'MarkerFaceColor', [0.7 0.7 0.7])
+            scatter(allData{1,t}.ROIcenters(allData{1,t}.uniqueHighCorrROI, 1), allData{1,t}.ROIcenters(allData{1,t}.uniqueHighCorrROI, 2), 150, 'o', 'filled', 'MarkerEdgeColor', 'y', 'MarkerFaceColor', 'y')
             hold off
+            set(findobj(gcf, 'type','axes'), 'Visible','off')
             xlim([0 500])
             ylim([0 500])
             fig_filename = sprintf('tectum shaped plot of highcorr ROIs tad %d stage %d MS', allData{1,t}.expnum, allData{1,t}.stage)
@@ -40,9 +41,10 @@ for t = 1:length(allData)
         if isfield(allData{1,t}, 'correlated_ROIs_dff0_V_common_AROI')
             figure;
             hold on
-            scatter(allData{1,t}.ROIcenters(allData{1,t}.resp_ROIs, 1), allData{1,t}.ROIcenters(allData{1,t}.resp_ROIs, 2), 65, 'o', 'MarkerEdgeColor', [0.5 0.5 0.5])
-            scatter(allData{1,t}.ROIcenters(allData{1,t}.uniqueHighCorrROI_V, 1), allData{1,t}.ROIcenters(allData{1,t}.uniqueHighCorrROI_V, 2), 100, 'o', 'filled', 'MarkerEdgeColor', 'r', 'MarkerFaceColor', 'r')
+            scatter(allData{1,t}.ROIcenters(allData{1,t}.resp_ROIs, 1), allData{1,t}.ROIcenters(allData{1,t}.resp_ROIs, 2), 150, 'o', 'filled', 'MarkerFaceColor', [0.7 0.7 0.7])
+            scatter(allData{1,t}.ROIcenters(allData{1,t}.uniqueHighCorrROI_V, 1), allData{1,t}.ROIcenters(allData{1,t}.uniqueHighCorrROI_V, 2), 150, 'o', 'filled', 'MarkerEdgeColor', 'r', 'MarkerFaceColor', 'r')
             hold off
+            set(findobj(gcf, 'type','axes'), 'Visible','off')
             xlim([0 500])
             ylim([0 500])
             fig_filename = sprintf('tectum shaped plot of highcorr ROIs tad %d stage %d V', allData{1,t}.expnum, allData{1,t}.stage)
@@ -70,11 +72,12 @@ for t = 1:length(allData)
         if isfield(allData{1,t}, 'correlated_ROIs_dff0_M_common_AROI')
             figure;
             hold on
-            scatter(allData{1,t}.ROIcenters(allData{1,t}.resp_ROIs, 1), allData{1,t}.ROIcenters(allData{1,t}.resp_ROIs, 2), 65, 'o', 'MarkerEdgeColor', [0.5 0.5 0.5])
-            scatter(allData{1,t}.ROIcenters(allData{1,t}.uniqueHighCorrROI_M, 1), allData{1,t}.ROIcenters(allData{1,t}.uniqueHighCorrROI_M, 2), 100, 'o', 'filled', 'MarkerEdgeColor', 'b', 'MarkerFaceColor', 'b')
+            scatter(allData{1,t}.ROIcenters(allData{1,t}.resp_ROIs, 1), allData{1,t}.ROIcenters(allData{1,t}.resp_ROIs, 2), 150, 'o', 'filled', 'MarkerFaceColor', [0.7 0.7 0.7])
+            scatter(allData{1,t}.ROIcenters(allData{1,t}.uniqueHighCorrROI_M, 1), allData{1,t}.ROIcenters(allData{1,t}.uniqueHighCorrROI_M, 2), 150, 'o', 'filled', 'MarkerEdgeColor', 'b', 'MarkerFaceColor', 'b')
             hold off
             xlim([0 500])
             ylim([0 500])
+            set(findobj(gcf, 'type','axes'), 'Visible','off')
             fig_filename = sprintf('tectum shaped plot of highcorr ROIs tad %d stage %d M', allData{1,t}.expnum, allData{1,t}.stage)
             saveas(gcf, fig_filename, 'tiff')
             close;
@@ -127,24 +130,25 @@ for t = 1:length(allData)
                     figure;
                     hold on
                     %all resp_ROIs as gray empty circles
-                    scatter(allData{1,t}.ROIcenters(allData{1,t}.resp_ROIs, 1), allData{1,t}.ROIcenters(allData{1,t}.resp_ROIs, 2), 65, 'o', 'MarkerEdgeColor', [0.5 0.5 0.5])
+                    scatter(allData{1,t}.ROIcenters(allData{1,t}.resp_ROIs, 1), allData{1,t}.ROIcenters(allData{1,t}.resp_ROIs, 2), 150, 'o', 'filled', 'MarkerFaceColor', [0.7 0.7 0.7])
                     %all highcorr_M ROIs as blue filled circles
-                    scatter(allData{1,t}.ROIcenters(allData{1,t}.uniqueHighCorrROI_M, 1), allData{1,t}.ROIcenters(allData{1,t}.uniqueHighCorrROI_M, 2), 100, 'o', 'filled', 'MarkerEdgeColor', 'b', 'MarkerFaceColor', 'b')
+                    scatter(allData{1,t}.ROIcenters(allData{1,t}.uniqueHighCorrROI_M, 1), allData{1,t}.ROIcenters(allData{1,t}.uniqueHighCorrROI_M, 2), 150, 'o', 'filled', 'MarkerEdgeColor', 'b', 'MarkerFaceColor', 'b')
                     %all highcorr_V ROIs as red filled circles
-                    scatter(allData{1,t}.ROIcenters(allData{1,t}.uniqueHighCorrROI_V, 1), allData{1,t}.ROIcenters(allData{1,t}.uniqueHighCorrROI_V, 2), 100, 'o', 'filled', 'MarkerEdgeColor', 'r', 'MarkerFaceColor', 'r')
+                    scatter(allData{1,t}.ROIcenters(allData{1,t}.uniqueHighCorrROI_V, 1), allData{1,t}.ROIcenters(allData{1,t}.uniqueHighCorrROI_V, 2), 150, 'o', 'filled', 'MarkerEdgeColor', 'r', 'MarkerFaceColor', 'r')
                     %all highcorr_MS ROIs as yellow filled circles
-                    scatter(allData{1,t}.ROIcenters(allData{1,t}.uniqueHighCorrROI, 1), allData{1,t}.ROIcenters(allData{1,t}.uniqueHighCorrROI, 2), 100, 'o', 'filled', 'MarkerEdgeColor', 'y', 'MarkerFaceColor', 'y')
+                    scatter(allData{1,t}.ROIcenters(allData{1,t}.uniqueHighCorrROI, 1), allData{1,t}.ROIcenters(allData{1,t}.uniqueHighCorrROI, 2), 150, 'o', 'filled', 'MarkerEdgeColor', 'y', 'MarkerFaceColor', 'y')
                     % all M and V highcorr ROIs as purple filled circles
-                    scatter(allData{1,t}.ROIcenters(allData{1,t}.highcorr_M_V, 1), allData{1,t}.ROIcenters(allData{1,t}.highcorr_M_V, 2), 100, 'o', 'filled', 'MarkerEdgeColor', [0.5 0 0.5], 'MarkerFaceColor', [0.5 0 0.5])
+                    scatter(allData{1,t}.ROIcenters(allData{1,t}.highcorr_M_V, 1), allData{1,t}.ROIcenters(allData{1,t}.highcorr_M_V, 2), 150, 'o', 'filled', 'MarkerEdgeColor', [0.5 0 0.5], 'MarkerFaceColor', [0.5 0 0.5])
                     % all M and MS highcorr ROIs as green filled circles
-                    scatter(allData{1,t}.ROIcenters(allData{1,t}.highcorr_M_MS, 1), allData{1,t}.ROIcenters(allData{1,t}.highcorr_M_MS, 2), 100, 'o', 'filled', 'MarkerEdgeColor', [0.4 0.5 0.2], 'MarkerFaceColor', [0.4 0.5 0.2])
+                    scatter(allData{1,t}.ROIcenters(allData{1,t}.highcorr_M_MS, 1), allData{1,t}.ROIcenters(allData{1,t}.highcorr_M_MS, 2), 150, 'o', 'filled', 'MarkerEdgeColor', [0.4 0.5 0.2], 'MarkerFaceColor', [0.4 0.5 0.2])
                     % all V and MS highcorr ROIs as orange filled circles
-                    scatter(allData{1,t}.ROIcenters(allData{1,t}.highcorr_M_V, 1), allData{1,t}.ROIcenters(allData{1,t}.highcorr_M_V, 2), 100, 'o', 'filled', 'MarkerEdgeColor', [1 0.6 0.2], 'MarkerFaceColor', [1 0.6 0.2])
+                    scatter(allData{1,t}.ROIcenters(allData{1,t}.highcorr_M_V, 1), allData{1,t}.ROIcenters(allData{1,t}.highcorr_M_V, 2), 150, 'o', 'filled', 'MarkerEdgeColor', [1 0.6 0.2], 'MarkerFaceColor', [1 0.6 0.2])
                     % all M, V and MS highcorr ROIs as black filled circles
-                    scatter(allData{1,t}.ROIcenters(allData{1,t}.highcorr_M_V_MS, 1), allData{1,t}.ROIcenters(allData{1,t}.highcorr_M_V_MS, 2), 100, 'o', 'filled', 'MarkerEdgeColor', 'k', 'MarkerFaceColor', 'k')
+                    scatter(allData{1,t}.ROIcenters(allData{1,t}.highcorr_M_V_MS, 1), allData{1,t}.ROIcenters(allData{1,t}.highcorr_M_V_MS, 2), 150, 'o', 'filled', 'MarkerEdgeColor', 'k', 'MarkerFaceColor', 'k')
                     hold off
-                    xlim([0 500])
-                    ylim([0 500])
+                    xlim([0 250])
+                    ylim([0 250])
+                    set(findobj(gcf, 'type','axes'), 'Visible','off')
                     fig_filename = sprintf('tectum shaped plot of highcorr ROIs tad %d (t%d) stage %d overlap', allData{1,t}.expnum, t, allData{1,t}.stage)
                     saveas(gcf, fig_filename, 'tiff')
                     close;
@@ -153,3 +157,139 @@ for t = 1:length(allData)
         end
     end
 end
+
+
+%% 46 to 49 basic comparison - better size labesl
+
+st46 = find(allRespROIs(:,28) == 46);
+st49 = find(allRespROIs(:,28) == 49);
+labels = {'area MS'; 'area V'; 'area M'; 'area NS'; 'peak MS'; 'peak V'; 'peak M'; 'peak NS'; ... 
+    'peak loc MS'; 'peak loc V'; 'peak loc M'; 'peak loc NS'; 'MSEnh peak'; 'unimax peak'; 'unimax stimtype'; ... 
+    'onset time MS'; 'onset time V'; 'onset time M'; 'onset time NS';...
+    'onset time SD MS'; 'onset time SD V'; 'onset time SD M'; 'onset time SD NS'; ...
+     'MSEnh num resp'; 'Uni bias num resp'};
+ 
+% make ECDF plots for each var 
+for i = 3:27
+    i = 15
+    s46 = allRespROIs(st46, i);
+    s49 = allRespROIs(st49, i);
+    figure;
+    hold on
+    ecdf(s46)
+
+    %set(h, 'Color', 'g')
+    ecdf(s49)
+    h = get(gca, 'children')
+    set(h, 'LineWidth', 4)
+    %j = get(gca, 'children')
+    %set(j, 'LineWidth', 3)
+    %set(j, 'Color', [0.5 0 0.5])
+    hold off
+    
+    title(sprintf('%s ECDF by stage', labels{i-2}))
+    xlabel(labels{i-2})
+    ylabel('ROI count')
+    set(gca, 'fontsize', 30)
+    xlim([-1 10])
+    %xticks([0 1 2 3 4 5 6 7])
+    fig_filename = sprintf('st 46 vs 49 ECDF of %s large labels', labels{i-2})
+    saveas(gcf, fig_filename, 'png')
+    close;
+end
+
+%% Does PCA differ by stage?
+% copied from PCA_loadings
+% This is based on Arseny's Elife paper plots
+% looking for PCA loadings
+
+% run PCA
+% doc: https://www.mathworks.com/help/stats/pca.html
+
+% get subset of allRespROIs data (eliminate area and peak loc)
+allRespROIs_PCAsub = allRespROIs(:, [7:9, 15, 16, 18:20, 26, 27]);
+labels_PCAsub = labels([5:7, 13, 14, 16:18, 24, 25])
+% run PCA on all responding ROIs together
+[coeff_all,score_all,latent_all,tsquared_all,explained_all,mu_all] = pca( [allRespROIs_PCAsub(:, :)]);
+% run PCA on each stage separately
+[coeff_46,score_46,latent_46,tsquared_46,explained_46,mu_46] = pca( [allRespROIs_PCAsub(st46, :)]);
+[coeff_49,score_49,latent_49,tsquared_49,explained_49,mu_49] = pca( [allRespROIs_PCAsub(st49, :)]);
+
+% the features (variables) put into allRespROIs(:, 3:27) are stored in labels
+
+% plot coefficients and data onto PC1 x PC2 space
+% https://www.mathworks.com/help/stats/biplot.html for documentation
+figure;
+biplot(coeff_all(:,1:2), 'Scores', score_all(:,1:2), 'VarLabels', labels_PCAsub)
+title('PCA loadings all data')
+set(gca, 'fontsize', 30)
+saveas(gcf, 'PCA loadings all data large labels', 'png')
+
+figure;
+biplot(coeff_46(:,1:2), 'Scores', score_46(:,1:2), 'VarLabels', labels_PCAsub)
+title('PCA Loadings st 46 only')
+set(gca, 'fontsize', 30)
+saveas(gcf, 'PCA loadings st46 large labels', 'png')
+
+figure;
+biplot(coeff_49(:,1:2), 'Scores', score_49(:,1:2), 'VarLabels', labels_PCAsub)
+title('PCA loadings st 49 only')
+set(gca, 'fontsize', 30)
+saveas(gcf, 'PCA loadings st49 large labels', 'png')
+
+
+% plot amount of variance explained by each PC
+figure;
+bar(explained_all)
+title('Variance explained by PC component all data')
+xlabel('components')
+ylabel('percent')
+set(gca, 'fontsize', 30)
+saveas(gcf, 'PCA var explained all data large labels', 'png')
+
+figure;
+bar(explained_46)
+title('Variance explained by PC component st46 only')
+xlabel('components')
+ylabel('percent')
+set(gca, 'fontsize', 30)
+saveas(gcf, 'PCA var explained st46 large labels', 'png')
+
+figure;
+bar(explained_49)
+title('Variance explained by PC component st49 only')
+xlabel('components')
+ylabel('percent')
+set(gca, 'fontsize', 30)
+saveas(gcf, 'PCA var explained st49 large labels', 'png')
+
+% plot MSEnh peak against PC1 value for all cells
+figure;
+scatter(score_all(:,1), allRespROIs_PCAsub(:,4))
+title('MSEnh peak vs PC1 All Data')
+xlabel('PC1')
+ylabel('MSEnh peak')
+xlim([-3 10])
+ylim([-1 10])
+set(gca, 'fontsize', 30)
+saveas(gcf, 'MSEnh peak vs PC1 allRespROIs large labels', 'png')
+
+figure;
+scatter(score_46(:,1), allRespROIs_PCAsub(st46,4))
+title('MSEnh peak vs PC1 St46 only')
+xlabel('PC1')
+ylabel('MSEnh peak')
+xlim([-3 10])
+ylim([-1 10])
+set(gca, 'fontsize', 30)
+saveas(gcf, 'MSEnh peak vs PC1 st46 only large labels', 'png')
+
+figure;
+scatter(score_49(:,1), allRespROIs_PCAsub(st49, 4))
+title('MSEnh peak vs PC1 St49 only')
+xlabel('PC1')
+ylabel('MSEnh peak')
+xlim([-3 10])
+ylim([-1 10])
+set(gca, 'fontsize', 30)
+saveas(gcf, 'MSEnh peak vs PC1 st49 only large labels', 'png')
